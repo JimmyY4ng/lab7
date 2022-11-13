a28ba757ea9dce7230dc0ac735c9aae7d1d391bc
Jimmy <Jyang321@my.bcit.ca>
2022-11-12 23:50:05 -0800

    final commit

--- seperate_folder/lab.js
+++ seperate_folder/lab.js
@@ -4,8 +4,24 @@ const courseList = [
     {code:"ORBG 1100",name:"Organizational Behaviour"},
 ]
 
+
 let input = ""
 do{
     input = prompt("Enter a BCIT course code (4-digit number):");
 }
 while(input.length !=4 || isNaN(Number(input)))
+
+for (const item in courseList) {
+    let courseNum = courseList[item].code.slice(5)
+    let courseChar = courseList[item].code.slice(0,4)
+    if (courseNum === input){
+        console.log(`Yes I am taking the course:${courseChar} - ${courseNum}`)
+        break
+    }
+    else {
+            console.log(`Success ${input}`);
+        
+        }
+    
+}
+
